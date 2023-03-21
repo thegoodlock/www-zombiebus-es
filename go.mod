fe0b259b73cf4843c60375146f2841245b1aa8e5
Maximo Cuadros <mcuadros@gmail.com>
2023-03-21 19:05:50 +0100

    change logo

--- go.mod
+++ go.mod
@@ -21,4 +21,6 @@ require (
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"net/http"
 	"os"
@@ -9,11 +10,14 @@ import (
 	"github.com/kelseyhightower/envconfig"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	gomail "gopkg.in/mail.v2"
 )
 
 type Config struct {
-	Addr string `envconfig:"PORT"`
-	Live bool   `envconfig:"LIVE"`
+	Addr     string `envconfig:"PORT"`
+	Live     bool   `envconfig:"LIVE"`
+	SMTPPass string `envconfig:"SMTP_USER" default:"reservas@zombiebus.es"`
+	SMTPUser string `envconfig:"SMTP_USER" default:"fQbgMFf4Dk6SsS2"`
 }
 
 //go:embed static
@@ -36,12 +40,38 @@ func main() {
 	var cfg Config
 	envconfig.MustProcess("web", &cfg)
 
+	fmt.Println(cfg.Live)
 	e := echo.New()
-	e.Pre(middleware.HTTPSRedirect())
+	if cfg.Live {
+		e.Pre(middleware.HTTPSRedirect())
+	}
+
 	e.GET("/*", echo.WrapHandler(http.FileServer(getFileSystem(cfg.Live))))
 	e.GET("/hello", func(c echo.Context) error {
+		sendEmail(&cfg)
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
 	e.Logger.Fatal(e.Start(":" + cfg.Addr))
+}
+
+func sendEmail(cfg *Config) {
+	fmt.Println(cfg.SMTPPass, cfg.SMTPUser)
+	m := gomail.NewMessage()
+	m.SetHeader("From", "from@gmail.com")
+	m.SetHeader("To", "to@example.com")
+	m.SetHeader("Subject", "Gomail test subject")
+	m.SetBody("text/plain", "This is Gomail test body")
+
+	d := gomail.NewDialer("smtp.gmail.com", 587, cfg.SMTPUser, cfg.SMTPUser)
+
+	//d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	// Now send E-Mail
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	return
 }
